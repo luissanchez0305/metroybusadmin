36263140ab8ed3bad1ce3f8db1dbf95312e45bc1
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-02-14 17:02:22 -0500

    titulos y slides sin mapa

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
 // The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
 var homePage =
     '<div>' +
-        '<div class="header"><h1>Page Slider</h1></div>' +
+        '<div class="header"><h1>Administrador</h1></div>' +
         '<div class="scroller">' +
             '<ul class="list">' +
                 '<li><a href="#page1"><strong>Paradas</strong></a></li>' +
@@ -17,10 +17,10 @@ var homePage =
 
 var detailsPage =
     '<div>' +
-        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="header"><a href="#" class="btn">Atras</a><h1>{{title}}</h1></div>' +
         '<div class="scroller">' +
             '<div class="robot">' +
-                '<img src="images/{{img}}" width="150" height="150"/>' +
+                '<img src="images/{{img}}" width="50" height="50"/>' +
                 '<h2>{{name}}</h2>' +
                 '<p>{{description}}</p>' +
             '</div>' +
@@ -37,10 +37,10 @@ function route(event) {
         hash = window.location.hash;
 
     if (hash === "#page1") {
-        page = merge(detailsPage, {img: "Bus-icon.jpg", name: "Build Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        page = merge(detailsPage, {img: "Bus-icon.jpg", title: "Paradas", name: "Presione en el mapa donde esta la parada", description: "{{Aqui va el mapa}}"});
 //        slider.slide($(page), "right");
     } else if (hash === "#page2") {
-        page = merge(detailsPage, {img: "busroute.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        page = merge(detailsPage, {img: "busroute.jpg", title: "Rutas", name: "Trace la ruta", description: "{{Aqui va el mapa}}"});
 //        slider.slide($(page), "right");
     } /*else if (hash === "#page3") {
         page = merge(detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
