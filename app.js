6de834b0fd7f682b72ce953a0203cb36177365bb
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-02-14 16:50:17 -0500

    cambio de iconos

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var homePage =
         '<div class="header"><h1>Page Slider</h1></div>' +
         '<div class="scroller">' +
             '<ul class="list">' +
-                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
-                '<li><a href="#page2"><strong>Medi Bot</strong></a></li>' +
-                '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
+                '<li><a href="#page1"><strong>Paradas</strong></a></li>' +
+                '<li><a href="#page2"><strong>Rutas</strong></a></li>' +
+                '<li style="display:none;"><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
             '</ul>' +
         '</div>' +
     '</div>';
@@ -20,7 +20,7 @@ var detailsPage =
         '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
         '<div class="scroller">' +
             '<div class="robot">' +
-                '<img src="images/{{img}}"/>' +
+                '<img src="images/{{img}}" width="150" height="150"/>' +
                 '<h2>{{name}}</h2>' +
                 '<p>{{description}}</p>' +
             '</div>' +
@@ -37,15 +37,15 @@ function route(event) {
         hash = window.location.hash;
 
     if (hash === "#page1") {
-        page = merge(detailsPage, {img: "buildbot.jpg", name: "Build Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        page = merge(detailsPage, {img: "Bus-icon.jpg", name: "Build Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
     } else if (hash === "#page2") {
-        page = merge(detailsPage, {img: "medibot.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
+        page = merge(detailsPage, {img: "busroute.jpg", name: "Medi Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
-    } else if (hash === "#page3") {
+    } /*else if (hash === "#page3") {
         page = merge(detailsPage, {img: "ripplebot.jpg", name: "Ripple Bot", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."});
 //        slider.slide($(page), "right");
-    }
+    }*/
     else {
         page = homePage;
 //        slider.slide($(homePage), "left");
